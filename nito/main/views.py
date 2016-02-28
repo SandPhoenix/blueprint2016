1a354265d8e7f8a8623211c0559520b3af0681b2
Matteo Sandrin <matteo.sandrin@hotmai.it>
2016-02-28 12:44:15 -0500

    Added new views code

--- nito/main/views.py
+++ nito/main/views.py
@@ -6,24 +6,54 @@ import hashlib
 # Create your views here.
 
 def index(request):
-	if not request.COOKIES.has_key('login'):
+	u = checkCookies(request)
+	if u  == False:
 		return signup(request)
 	else:
-		for u in User.objects.all():
-			hash_name = hashlib.sha512(request.COOKIES['login']).hexdigest()
-			if u.hash_name == hash_name
-				context = {'user' : u}
-				return render(request,'main/index.html',context)
-			else:
-				return signup(request)
+		context = {'user' : u}
+		return render(request,'main/index.html',context)
 
 def signup(request):
-	for u in User.objects.all():
-			if u.hash_name == request.COOKIES['login']
-				context = {'user' : u}
+	if request.POST.has_key('login'):
+		hash_name = hashlib.sha512(request.POST['login']).hexdigest()
+		for u in User.objects.all():
+			if u.hash_name == hash_name:
+				return error(request,"The username is aready taken.")
+		else:
+			new_user = User()
+			new_user.setName(request.POST['login'])
+			new_user.save()
+			return randompost(request)
+	else:
+		return error(request,"Missing Parameter")
 
 def randompost(request):
-	posts = Post.objects.all()
-	p = posts[random.randint(0,len(posts))]
-	context = {'post':p}
-	return render(request,'main/randompost.html',context)
+	if checkCookies(request) != False:
+		posts = Post.objects.all()
+		p = posts[random.randint(0,len(posts))]
+		context = {'post':p}
+		return render(request,'main/randompost.html',context)
+	else:
+		return error(request,"Not Authorized")
+
+def submitpost(request):
+	u = checkCookies(request)
+	if u != False:
+		
+
+
+def error(request,string):
+	context = {'string' : string }
+	return render(request,'main/error.html',context)
+
+def checkCookies(request):
+	if request.COOKIES.has_key('login'):
+		hash_name = hashlib.sha512(request.COOKIES['login']).hexdigest()
+		for u in User.objects.all():
+			if u.hash_name == hash_name:
+				return u
+		else:
+			return False
+	else:
+		return False
+
